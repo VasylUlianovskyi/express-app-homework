53989cda5bea62ec021a2f798cc979c025f1de69
Vasyl Ulianovskyi <vasia.kit434@gmail.com>
2024-09-04 00:54:22 +0300

    Add the rest of the handlers from the homework

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require('express');
-const { v4: uuidv4 } = require('uuid');
-const { createTask } = require('./controllers/taskControllers');
+const {
+  createTask,
+  getTasks,
+  getTaskById,
+  updateTask,
+  deleteTask,
+} = require('./controllers/taskControllers');
 
 const app = express();
 app.use(express.json());
 
+app.get('/tasks', getTasks);
 app.post('/tasks', createTask);
+app.get('/tasks/:id', getTaskById);
+app.patch('/tasks/:id', updateTask);
+app.delete('/tasks/:id', deleteTask);
 
 module.exports = app;
--- controllers/taskControllers.js
+++ controllers/taskControllers.js
@@ -2,27 +2,65 @@ const { v4: uuidv4 } = require('uuid');
 
 const tasks = [
   {
-    id: uuidv4,
+    id: 'fec5a357',
     body: 'Complete the React project',
     deadline: '2024-09-10',
     isDone: false,
   },
   {
-    id: uuidv4,
+    id: uuidv4(),
     body: 'Prepare for the meeting',
     deadline: '2024-09-05',
     isDone: true,
   },
   {
-    id: uuidv4,
+    id: uuidv4(),
     body: 'Buy groceries',
     deadline: '2024-09-07',
     isDone: false,
   },
 ];
 
+module.exports.getTasks = (req, res) => {
+  res.status(200).send(tasks);
+};
+
 module.exports.createTask = (req, res) => {
   const { body } = req;
   tasks.push({ ...body, id: uuidv4() });
   res.status(201).send(tasks[tasks.length - 1]);
+};
+
+module.exports.getTaskById = (req, res) => {
+  const { id } = req.params;
+  const foundTask = tasks.find(t => t.id === id);
+
+  if (!foundTask) {
+    return res.status(404).send('Task Not Found');
+  }
+  res.status(200).send(foundTask);
+};
+
+module.exports.updateTask = (req, res) => {
+  const {
+    body,
+    params: { id },
+  } = req;
+  const foundTaskIndex = tasks.findIndex(t => t.id === id);
+
+  if (foundTaskIndex === -1) {
+    return res.status(404).send('Task Not Found');
+  }
+  tasks[foundTaskIndex] = { ...tasks[foundTaskIndex], ...body };
+  res.status(200).send(tasks[foundTaskIndex]);
+};
+
+module.exports.deleteTask = (req, res) => {
+  const { id } = req.params;
+  const foundTask = tasks.findIndex(t => t.id === id);
+  if (foundTask === -1) {
+    return res.status(404).send('Task Not Found');
+  }
+  tasks.splice(foundTask, 1);
+  res.status(204).send();
 };
